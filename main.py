7eee24b7458728c47a43b30accde2c4f436df1d8
Trahaswi <102136210+Trahaswi@users.noreply.github.com>
2022-06-12 15:52:18 +0530

    uploaded files

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MainWindow(QMainWindow) :
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://duckduckgo.com'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Anuwin Browser')
+window = MainWindow()
+app.exec_()
